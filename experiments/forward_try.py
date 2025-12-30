145043d89bb90afdc7f8154f9431b616d60667db
shzmacmini <hbushz@icloud.com>
2025-12-30 21:56:30 +0800

    完成FTM的测量过程仿真

--- experiments/forward_try.py
+++ experiments/forward_try.py
@@ -1,10 +1,11 @@
+import time
 from scipy.io import loadmat
 import numpy as np
 from numpy import fft
 import matplotlib.pyplot as plt
-from utils.utils import image_center_region
-from utils.utils import ideal_lowpass_filter
-from utils.utils import led_array_dir_cos
+from ptych.utils import image_center_region
+from ptych.utils import ideal_lowpass_filter
+from ptych.utils import led_array_dir_cos
 
 ################################################################################
 # 理想像的参数
@@ -22,6 +23,8 @@ ideal_image_interval = 4e-3  # 以mm为长度单位
 ideal_image_interval = ideal_image_interval / 16  # 将采样间隔缩小16倍，组号加4
 # 理想像的频谱间隔
 ideal_spectrum_interval = 1 / (ideal_image_interval * ideal_rows)
+# 理想像的频谱
+ideal_image_fourier = fft.fftshift(fft.fft2(fft.ifftshift(ideal_image)))
 
 ################################################################################
 # 实际物镜的参数
@@ -39,7 +42,7 @@ print(f"实际物镜有限孔径对应的截止频率为: {cutoff_freq}")
 # LED阵列照明的参数
 ################################################################################
 # LED 矩阵的行列数目(奇数行，奇数列)
-led_rows, led_cols = 5, 5
+led_rows, led_cols = 7, 7
 # LED阵列两灯之间的间隔
 led_d = 3.133  # 以mm为单位
 # LED灯板到样本的距离
@@ -50,53 +53,58 @@ led_dir_cos = led_array_dir_cos(led_rows, led_cols, distance_led2sample)
 # LED照明的空间频率矩阵
 led_frequency = led_dir_cos / wavelength
 led_frequency = led_frequency / ideal_spectrum_interval  # 以理想像的频谱间隔为单位
-print(f"LED照明最大频率间隔为: {led_frequency[led_rows//2+1, led_cols//2, 0]}")
+# LED照明最大频率间隔, 应为从中心LED到四邻接LED
+print(f"LED照明最大频率间隔为: {led_frequency[led_rows // 2 + 1, led_cols // 2, 0]}")
 
-# 生成理想低通滤波器
-lowpass_filter = ideal_lowpass_filter(
-    (ideal_rows, ideal_cols),
-    cutoff_freq,
-    (
-        led_frequency[led_rows // 2 + 1, led_cols // 2, 0],
-        led_frequency[led_rows // 2 + 1, led_cols // 2, 1],
-    ),
-)
-
-# 低通滤波
-ideal_image_fourier = fft.fftshift(fft.fft2(fft.ifftshift(ideal_image)))
-real_image_fourier = ideal_image_fourier * lowpass_filter
-# 逆傅里叶变换, 得到实际像
-real_image = fft.fftshift(fft.ifft2(fft.ifftshift(real_image_fourier)))
-
-
+################################################################################
 # 显示
-plt.figure(figsize=(6, 6))
-
+################################################################################
+plt.ion()
+fig, axs = plt.subplots(2, 2, figsize=(6, 6))
+# ----------- 第1行的两个静态图像 -----------#
 # 显示ideal image
-plt.subplot(2, 2, 1)
-plt.imshow(ideal_image, cmap="gray")
-plt.axis("off")
-plt.title("Ideal Image (Group No.+4)")
-
-# 显示spatial spectrum
-plt.subplot(2, 2, 2)
-plt.imshow(np.log(np.abs(ideal_image_fourier) + 1), cmap="gray")
-plt.axis("off")
-plt.title("Spatial Spectrum")
-
+axs[0, 0].imshow(ideal_image, cmap="gray")
+axs[0, 0].axis("off")
+axs[0, 0].set_title("Ideal Image (Group No.+4)")
+# 显示ideal image的频谱
+axs[0, 1].imshow(np.log(np.abs(ideal_image_fourier) + 1), cmap="gray")
+axs[0, 1].axis("off")
+axs[0, 1].set_title("Spatial Spectrum")
+# ----------- 第2行的两个动态图像 -----------#
 # 显示滤波后的频谱
-plt.subplot(2, 2, 3)
-plt.imshow(
-    np.log(np.abs(ideal_image_fourier) + 1) * (lowpass_filter + 1),
+im_spec = axs[1, 0].imshow(
+    np.log(np.abs(ideal_image_fourier) + 1),
     cmap="gray",
 )
-plt.axis("off")
-plt.title("Filtered Spectrum")
-
+axs[1, 0].axis("off")
+axs[1, 0].set_title("Filtered Spectrum")
 # 显示real image
-plt.subplot(2, 2, 4)
-plt.imshow(np.abs(real_image), cmap="gray")
-plt.axis("off")
-plt.title("Real Image")
+im_real = axs[1, 1].imshow(ideal_image, cmap="gray")
+axs[1, 1].axis("off")
+axs[1, 1].set_title("Real Image")
 
+# ----------- 更新动态图像 -----------#
+for m in range(led_rows):
+    for n in range(led_cols):
+        # 生成理想低通滤波器
+        lowpass_filter = ideal_lowpass_filter(
+            (ideal_rows, ideal_cols),
+            cutoff_freq,
+            (led_frequency[m, n, 0], led_frequency[m, n, 1]),
+        )
+        # 低通滤波
+        real_image_fourier = ideal_image_fourier * lowpass_filter
+        # 逆傅里叶变换, 得到实际像
+        real_image = fft.fftshift(fft.ifft2(fft.ifftshift(real_image_fourier)))
+
+        # 显示滤波后的频谱
+        im_spec.set_data(
+            np.log(np.abs(ideal_image_fourier) + 1) * (0.2 + lowpass_filter)
+        )
+        # 显示real image
+        im_real.set_data(np.abs(real_image))
+
+        plt.pause(0.5)
+
+plt.ioff()
 plt.show()
